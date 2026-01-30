99ba08c4c88fc80f0ad26a7b5769611db8d6f2fc
Blur12300 <pottingerjake@gmail.com>
2026-01-30 22:41:46 +0000

    Added Road with basic Movement and Mirrors

--- PAPD_GAME/Assets/Scripts/Bike_Controller.cs
+++ PAPD_GAME/Assets/Scripts/Bike_Controller.cs
@@ -4,7 +4,13 @@ using UnityEngine.InputSystem;
 public class Bike_Controller : MonoBehaviour
 {
     public CharacterController cc;
-    public float speed;
+    public float frontSpeed;
+    public float sideSpeed;
+
+    public float maxFront;
+    public float maxSide;
+
+    public float accel = 1.02f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,8 +23,45 @@ public class Bike_Controller : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)) 
         {
-
-            cc.Move(new Vector3(0, 0, speed));
+            cc.Move(new Vector3(0, 0, frontSpeed));
+            if (frontSpeed < maxFront)
+            {
+                frontSpeed = frontSpeed * accel;
+            }
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            cc.Move(new Vector3(-sideSpeed, 0, 0));
+            if (sideSpeed < maxSide)
+            {
+                sideSpeed = sideSpeed * accel;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            cc.Move(new Vector3(0, 0, -frontSpeed));
+            if (frontSpeed < maxFront)
+            {
+                frontSpeed = frontSpeed * accel;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            cc.Move(new Vector3(sideSpeed, 0, 0));
+            if (sideSpeed < maxSide)
+            {
+                sideSpeed = sideSpeed * accel;
+            }
+        }
+        
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) 
+        {
+            frontSpeed = 1f;
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            sideSpeed = 0.5f;
+        }
+
     }
 }
